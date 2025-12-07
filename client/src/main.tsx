6e578be869b743d2cffdc1d7090d6a95f62f81d8
sudhirVoid <bhandarisudhir4274@gmail.com>
2025-12-07 18:47:09 +0530

    preconnect to cloudinary and prefetch banner to decrease largest contentful paint.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -3,4 +3,13 @@ import App from "./App";
 import "./index.css";
 import './i18n';
 
+import { queryClient } from "./lib/queryClient";
+import { bannersService } from "./services/banners.service";
+
+// Prefetch banners immediately to improve LCP
+queryClient.prefetchQuery({
+    queryKey: ["active-banners"],
+    queryFn: () => bannersService.getActiveBanners(),
+});
+
 createRoot(document.getElementById("root")!).render(<App />);
